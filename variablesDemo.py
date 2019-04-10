a7808b1d1d0be80f22370b1808722772926feede
Sirisha Velampalli <sirisha.velampalli@gmail.com>
2019-04-10 19:48:52 +0530

    Add files via upload

---
+++ variablesDemo.py
@@ -0,0 +1,20 @@
+a=5
+print(a)
+a=9
+print(a)
+a='sirisha is working in the areas of NLP, DM'
+print(a)
+b=20
+print(b,a)
+c=22
+print(a,b,c)
+
+a = 99
+print(a)
+def newfunction():
+   global a
+   a=101
+print(a)
+newfunction()
+print(a)
+
