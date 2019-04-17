d471dc28317c3fdba0a7d0b5249602c8a0d66b48
Rekha Chakkaravarthy <40236522+rekhachakkaravarthy@users.noreply.github.com>
2019-04-17 11:04:57 -0400

    Update listDemo.py

--- listDemo.py
+++ listDemo.py
@@ -13,9 +13,9 @@ list1.insert(6,"pansies")
 --- added by rekha ---
 list1.insert(2,102.6)
 
-roses, lillies, 102.6, jasmine, pansies
-
----------------------
+-- > o/p --> roses, lillies, 102.6, jasmine, pansies
+              0       1       3       -2       -1
+----------------------------------------------------
 print(list1[0])
 print(list1[-1])
 print(list1)
