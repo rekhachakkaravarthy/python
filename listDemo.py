0fb3ef7f6100676efa67bfa199dc0603a3a0b4fe
Sirisha Velampalli <sirisha.velampalli@gmail.com>
2019-04-17 20:21:36 +0530

    Update listDemo.py

--- listDemo.py
+++ listDemo.py
@@ -12,6 +12,7 @@ print(list1)
 list1.insert(6,"pansies")
 --- added by rekha ---
 list1.insert(2,102.6)
+roses lillies 102.6 jasmines pansies
 ---------------------
 print(list1[0])
 print(list1[-1])
